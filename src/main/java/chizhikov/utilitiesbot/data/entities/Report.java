48ab410a070ae27afa6849268a5085e324ac4a08
Chizhikov <chizhikov.alexey@icloud.com>
2021-06-13 01:19:23 +0300

    Report refactoring

--- src/main/java/chizhikov/utilitiesbot/data/entities/Report.java
+++ src/main/java/chizhikov/utilitiesbot/data/entities/Report.java
@@ -3,40 +3,35 @@ package chizhikov.utilitiesbot.data.entities;
 import java.time.LocalDate;
 
 public class Report {
-    private Integer electricity;
-    private Integer hotWaterBath;
-    private Integer coldWaterBath;
-    private Integer hotWaterKitchen;
-    private Integer coldWaterKitchen;
-    private Integer drainage;
-    private LocalDate date;
-
-    private final MonthData firstMonthData;
-    private final MonthData secondMonthData;
-
+    private final Integer electricity;
+    private final Integer hotWaterBath;
+    private final Integer coldWaterBath;
+    private final Integer hotWaterKitchen;
+    private final Integer coldWaterKitchen;
+    private final Integer drainage;
+    private final LocalDate date;
     private final Tariff tariff;
 
     public Report(MonthData firstMonthData, MonthData secondMonthData, Tariff tariff) {
         this.tariff = tariff;
-        this.firstMonthData = firstMonthData;
-        this.secondMonthData = secondMonthData;
 
         electricity = secondMonthData.getElectricity() - firstMonthData.getElectricity();
         hotWaterBath = secondMonthData.getHotWaterBath() - firstMonthData.getHotWaterBath();
         coldWaterBath = secondMonthData.getColdWaterBath() - firstMonthData.getColdWaterBath();
-        hotWaterKitchen= secondMonthData.getHotWaterKitchen() - firstMonthData.getHotWaterKitchen();
+        hotWaterKitchen = secondMonthData.getHotWaterKitchen() - firstMonthData.getHotWaterKitchen();
         coldWaterKitchen = secondMonthData.getColdWaterKitchen() - firstMonthData.getColdWaterKitchen();
         drainage = hotWaterBath + coldWaterBath + hotWaterKitchen + coldWaterKitchen;
         date = secondMonthData.getDate();
     }
 
     public String getShortReport() {
-        return  """
+        return """
                 Отчёт от %s
-                Расход электроэнергии: %d -> %.2f рублей;
-                Расход горячей воды: %d -> %.2f рублей;
-                Расход холодной воды: %d -> %.2f рублей;
-                
+                Электроэнергия: %d -> %.2f рублей;
+                Горячая вода: %d -> %.2f рублей;
+                Холодная вода: %d -> %.2f рублей;
+                Водоотведение: %d -> %.2f рублей;
+                                
                 ИТОГО: %.2f рублей.
                 """
                 .formatted(
@@ -44,6 +39,7 @@ public class Report {
                         electricity, electricity * tariff.getElectricity(),
                         hotWaterBath + hotWaterKitchen, (hotWaterBath + hotWaterKitchen) * tariff.getHotWater(),
                         coldWaterBath + coldWaterKitchen, (coldWaterBath + coldWaterKitchen) * tariff.getColdWater(),
+                        drainage, drainage * tariff.getDrainage(),
                         electricity * tariff.getElectricity() + (hotWaterBath + hotWaterKitchen) * tariff.getHotWater() + (coldWaterBath + coldWaterKitchen) * tariff.getColdWater()
                 );
     }

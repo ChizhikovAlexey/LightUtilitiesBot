6974675b5b32aedf13c1e4baa6bb803f2cad852f
Chizhikov <chizhikov.alexey@icloud.com>
2021-06-19 14:23:39 +0300

    + Lombok

--- src/main/java/chizhikov/utilitiesbot/data/entities/MonthData.java
+++ src/main/java/chizhikov/utilitiesbot/data/entities/MonthData.java
@@ -1,12 +1,16 @@
 package chizhikov.utilitiesbot.data.entities;
 
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.time.LocalDate;
 
 @NoArgsConstructor
 @AllArgsConstructor
+@Getter
+@Setter
 public class MonthData {
     private Integer id;
     private Integer electricity;
@@ -15,62 +19,6 @@ public class MonthData {
     private Integer hotWaterKitchen;
     private Integer coldWaterKitchen;
     private LocalDate date;
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public Integer getElectricity() {
-        return electricity;
-    }
-
-    public void setElectricity(Integer electricity) {
-        this.electricity = electricity;
-    }
-
-    public Integer getHotWaterBath() {
-        return hotWaterBath;
-    }
-
-    public void setHotWaterBath(Integer hotWaterBath) {
-        this.hotWaterBath = hotWaterBath;
-    }
-
-    public Integer getColdWaterBath() {
-        return coldWaterBath;
-    }
-
-    public void setColdWaterBath(Integer coldWaterBath) {
-        this.coldWaterBath = coldWaterBath;
-    }
-
-    public Integer getHotWaterKitchen() {
-        return hotWaterKitchen;
-    }
-
-    public void setHotWaterKitchen(Integer hotWaterKitchen) {
-        this.hotWaterKitchen = hotWaterKitchen;
-    }
-
-    public Integer getColdWaterKitchen() {
-        return coldWaterKitchen;
-    }
-
-    public void setColdWaterKitchen(Integer coldWaterKitchen) {
-        this.coldWaterKitchen = coldWaterKitchen;
-    }
-
-    public LocalDate getDate() {
-        return date;
-    }
-
-    public void setDate(LocalDate date) {
-        this.date = date;
-    }
 
     @Override
     public String toString() {
--- src/main/java/chizhikov/utilitiesbot/data/entities/Tariff.java
+++ src/main/java/chizhikov/utilitiesbot/data/entities/Tariff.java
@@ -1,12 +1,16 @@
 package chizhikov.utilitiesbot.data.entities;
 
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.time.LocalDate;
 
 @NoArgsConstructor
 @AllArgsConstructor
+@Getter
+@Setter
 public class Tariff {
     private Integer id;
     private Double electricity;
@@ -14,54 +18,6 @@ public class Tariff {
     private Double coldWater;
     private Double drainage;
     private LocalDate date;
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public Double getElectricity() {
-        return electricity;
-    }
-
-    public void setElectricity(Double electricity) {
-        this.electricity = electricity;
-    }
-
-    public Double getHotWater() {
-        return hotWater;
-    }
-
-    public void setHotWater(Double hotWater) {
-        this.hotWater = hotWater;
-    }
-
-    public Double getColdWater() {
-        return coldWater;
-    }
-
-    public void setColdWater(Double coldWater) {
-        this.coldWater = coldWater;
-    }
-
-    public Double getDrainage() {
-        return drainage;
-    }
-
-    public void setDrainage(Double drainage) {
-        this.drainage = drainage;
-    }
-
-    public LocalDate getDate() {
-        return date;
-    }
-
-    public void setDate(LocalDate date) {
-        this.date = date;
-    }
 
     @Override
     public String toString() {
